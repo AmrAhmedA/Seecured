058ac13c21ee0e5859a2174e25db6223b6d499f9
AmrAhmedA <47694180+AmrAhmedA@users.noreply.github.com>
2020-06-18 00:39:23 +0200

    Updates

--- js/app.js
+++ js/app.js
@@ -3,13 +3,14 @@ App = {
     contracts: {},
     account: '0x0',
     hasVoted: false,
-
+    isEligable: false,
     init: function() {
         const metamaskInstalled = typeof window.web3 !== 'undefined';
         if (metamaskInstalled) {
             return App.initWeb3();
         } else {
             if (!metamaskInstalled) {
+                alert("Please Install MetaMask");
                 var loader = $("#loader");
                 loader.empty();
                 var result = `<h2 class='text-center'> Please Install MetaMask </h2> <img class='img-responsive center-block' src='images/metamask.png' alt='Seecured Logo' height='250' width='150'></img>`
@@ -18,43 +19,48 @@ App = {
             }
         }
     },
-    //Function To Initialize our Connection to the Client Side Application
+    //Function To Initialize Seecured Connection to the Client Side Application
     initWeb3: function() {
         if (typeof web3 !== 'undefined') {
-            // If a web3 instance is already provided by Meta Mask.
+            // If a web3 instance is already provided by Meta Mask (A node has the ability to view and interact with Ethereum)
             App.web3Provider = web3.currentProvider;
+            // App.web3Provider = new Web3.providers.HttpProvider("https://kovan.infura.io/v3/cbc6700679974ee0bb0c6c62a480438c");
             ethereum.enable();
             web3 = new Web3(web3.currentProvider);
+            // web3 = new Web3(new Web3.providers.HttpProvider("https://kovan.infura.io/v3/cbc6700679974ee0bb0c6c62a480438c"));
         } else {
             alert("Please Install MetaMask");
-            // Specify default instance if no web3 instance provided
+            // Specify default instance if no web3 instance provided - Ganache as default provider
             App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
             ethereum.enable();
             web3 = new Web3(App.web3Provider);
         }
         return App.initContract();
     },
-
     initContract: function() {
         $.getJSON("Election.json", function(election) {
             // Instantiate a new truffle contract from the artifact
             App.contracts.Election = TruffleContract(election);
             // Connect provider to interact with contract
             App.contracts.Election.setProvider(App.web3Provider);
-            App.listenForEvents();
             return App.render();
         });
     },
-    listenForEvents: function() {
+    votedEventListener: function() {
         App.contracts.Election.deployed().then(function(instance) {
             instance.votedEvent({}, {
-                fromBlock: 'latest',
-                toBlock: 'latest'
+                // subscribe for voted events since the contract creation 
+                // fromBlock: 0,
+                // toBlock: 'latest'
             }).watch(function(error, event) {
-                console.log("event triggered", event);
-                // Reload when a new vote is recorded
-                console.log("Hey Amr, It worked");
-                App.render();
+                if (!error) {
+                    console.log("event triggered", event);
+                    // Reload when a new vote is recorded
+                    console.log("Hey Amr, It worked");
+                    App.render();
+                } else {
+                    console.log("Error", error);
+                }
             });
         });
     },
@@ -93,7 +99,6 @@ App = {
                     var committee = candidate[3];
                     var voteCount = candidate[4];
 
-
                     // Render candidate Result
                     var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</th><td>" + studentID + "</td><td>" + committee + "</td><td>" + voteCount + "</td></tr>";
                     candidatesResults.append(candidateTemplate);
@@ -104,13 +109,15 @@ App = {
                 });
             }
             return electionInstance.voters(App.account).then(function(voters) {
+                isEligable = voters[0];
                 hasVoted = voters[1];
             })
         }).then(function() {
+            console.log("----Checking Voter Status----");
+            console.log("Eligibility to vote: " + isEligable);
+            console.log("Voted: " + hasVoted);
             // Do not allow a user to vote
-            console.log("Hello");
-
-            if (hasVoted) {
+            if (!isEligable || hasVoted) {
                 $('form').hide();
             }
             loader.hide();
@@ -130,6 +137,7 @@ App = {
             // Wait for votes to update
             $("#content").hide();
             $("#loader").show();
+            App.votedEventListener();
         }).catch(function(err) {
             console.error(err);
         });
