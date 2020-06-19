88e00e16c89ec7c6ec7c4e7da24b2b7fceb24dfd
AmrAhmedA <47694180+AmrAhmedA@users.noreply.github.com>
2020-06-19 21:19:29 +0200

    Updating Contract + Adding Timer

--- election/src/js/app.js
+++ election/src/js/app.js
@@ -103,6 +103,9 @@ App = {
             }
         });
 
+        // Load Timer Data
+        App.electionTimer();
+
         // Load contract data
         App.contracts.Election.deployed().then(function(instance) {
             electionInstance = instance;
@@ -139,6 +142,7 @@ App = {
             console.log("----Checking Voter Status----");
             console.log("Eligibility to vote: " + isEligable);
             console.log("Voted: " + hasVoted);
+
             // Do not allow a user to vote
             if (!isEligable || hasVoted) {
                 $('form').hide();
@@ -148,6 +152,58 @@ App = {
         }).catch(function(error) {
             console.warn(error);
             alert("Sorry Amr, You Need to Focus More");
+        });
+    },
+    electionTimer: function() {
+        App.contracts.Election.deployed().then(function(instance) {
+            electionInstance = instance;
+            return electionInstance.electionDate();
+        }).then(function(electionDate) {
+            console.log("Election End-Date: " + electionDate);
+
+            // extracting election day
+            var electionDay = electionDate.slice(0, 2);
+
+            // extracting election month    
+            var electionMonth = electionDate.slice(3, 5);
+
+            // extracting election year
+            var electionYear = electionDate.slice(6, 10);
+
+            console.log(electionDay);
+            // set the date we're counting down to
+            var target_date = new Date(electionDay + ',' + electionMonth + ',' + electionYear).getTime();
+
+            // variables for time units
+            var days, hours, minutes, seconds;
+
+            // get tag element
+            var countdown = document.getElementById('countdown');
+
+            // update the tag with id "countdown" every 1 second
+            setInterval(function() {
+
+                // find the amount of "seconds" between now and target
+                var current_date = new Date().getTime();
+                var seconds_left = (target_date - current_date) / 1000;
+
+                // do some time calculations
+                days = parseInt(seconds_left / 86400);
+                seconds_left = seconds_left % 86400;
+
+                hours = parseInt(seconds_left / 3600);
+                seconds_left = seconds_left % 3600;
+
+                minutes = parseInt(seconds_left / 60);
+                seconds = parseInt(seconds_left % 60);
+
+                // format countdown string + set tag value
+                countdown.innerHTML = '<span class="days">' + days + ' <label>Days</label></span> <span class="hours">' + hours + ' <label>Hours</label></span> <span class="minutes">' +
+                    minutes + ' <label>Minutes</label></span> <span class="seconds">' + seconds + ' <label>Seconds</label></span>';
+
+            }, 1000);
+        }).catch(function(error) {
+            console.warn(error);
         });
     },
     castVote: function() {
